17d13ce2fc3ea905c48405c698b4d7f742d4698d
monzur rahman <89568953+monzurrahman@users.noreply.github.com>
2024-10-04 22:55:07 +0600

    Update file-upload-db.php
    Name, Email, Phone, Address, File upload fields are prepared. DB fields are also extended accordingly. htmlspecialchars is used to sanitize the input form data. required named tag-element added in html input form fields. max length added in input-type number. we used maxlength tag/element in textarea. i have tested that the Name, Email, Phone, Address, File upload fields are working smoothly form my end. the info is also being stored in db.

--- file-upload-db.php
+++ file-upload-db.php
@@ -10,12 +10,18 @@
 	
 	<form action="" method="post" enctype="multipart/form-data">
 		<label><h1>For submission to staroe and retrieve in database</h1></label>
-		<input type="text" name="text_input" > <br/>
+
+		<input type="text" name="text_input" id="name" placeholder="Name"> <br/>
+		<input type="email" name="email" id="email" placeholder="Email" required> <br/>
+		<input type="number" name="phone" id="phone" placeholder="Phone Number" min="1" max="1999999999" required> <br/>
+		<textarea name="address_text" id="address_t" placeholder="Enter your address by 100 characters" maxlength="100" ></textarea><br/>
 		<input type="file" name = "file_name_var"><br/><br/> 
 		<input type="submit" name = "button_var">
+
 	</form>
 <?php
 
+// Database issue- DB connection setup
 $db_name = "my_new_db_name";
 $db_user = "root";
 $db_possord = "";
@@ -23,21 +29,28 @@ $db_hostname = "localhost";
 $db_connect = mysqli_connect($db_hostname, $db_user, $db_possord, $db_name );
 
 if (isset($_POST['button_var']))
-		{
+		{ 
 
-					$name_var = $_POST['text_input'];
-					$file_name_var = $_FILES['file_name_var']['name'];
-					$file_temp_name = $_FILES['file_name_var']['tmp_name'];  // i did mistake here writing tem instead of tmp , careful. Error- Undefined array key 
-					$file_type = $_FILES['file_name_var']['type'];
-					$file_directory = "uploads/".basename($file_name_var); // i did 3 mistake here- omiting .basename..not a directory type error.
+			// Form related data taken here-
+				$input_name    = htmlspecialchars($_POST['text_input']);
+				$input_email   = htmlspecialchars($_POST['email']);
+				$input_phone   = htmlspecialchars($_POST['phone']);
+				$input_address = htmlspecialchars($_POST['address_text']);
+			//  $_SERVER['HTTP_REFERER'] can be applied to check if the form is actioned from it's self domain.. althoug it has vulnerable  issue
+			// the data of the form fields can be sanitize by htmlspecialchars — Convert special characters to HTML entities. i.e. > converts to &gt;
+					// $name_var = $_POST['text_input'];
+				$file_name_var = $_FILES['file_name_var']['name'];
+				$file_temp_name = $_FILES['file_name_var']['tmp_name'];  // i did mistake here writing tem instead of tmp , careful. Error- Undefined array key 
+				$file_type = $_FILES['file_name_var']['type'];
+				$file_directory = "uploads/".basename($file_name_var); // i did 3 mistake here- omiting .basename..not a directory type error.
 
 					if (move_uploaded_file($file_temp_name, $file_directory))
 					{
 
 					echo "File uploaded successfully"."<br/>";
 
-						$sql = "INSERT INTO uploads (file_name, file_path, file_type) VALUES ('$name_var', '$file_directory', '$file_type')"; 
-						// Got Fatal error: Uncaught mysqli_sql_exception...... when i wrote values without single quotation like,  VALUES ($name_var, $file_directory, $file_type)
+						$sql = "INSERT INTO uploads (person_name, person_email, person_phone, person_address, file_name, file_path, file_type) VALUES ('$input_name', '$input_email', '$input_phone', '$input_address', '$file_name_var', '$file_directory', '$file_type')"; 
+						// i did same mistake for the 3 times.. Got Fatal error: Uncaught mysqli_sql_exception...... when i wrote values without single quotation i.e.,  VALUES ($name_var, $file_directory, $file_type)
 
 						 	if(mysqli_query($db_connect, $sql))
 								{
@@ -53,10 +66,15 @@ if (isset($_POST['button_var']))
 </html>
 <?php
 /*
-MySql code goes here- 
+
+Second stage DB code goes here-
 
 CREATE TABLE `uploads` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `person_name` varchar(255) NOT NULL,
+  `person_email` varchar(255) NOT NULL,
+  `person_phone` varchar(255) NOT NULL,
+  `person_address` varchar(255),
   `file_name` varchar(255) NOT NULL,
   `file_path` varchar(255) NOT NULL,
   `file_type` varchar(50) NOT NULL,
